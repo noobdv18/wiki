c95323a6fe397812ac901a137244c08bad81fc55
Omkar Thite <101266411+noobdv18@users.noreply.github.com>
2022-11-27 12:49:10 +0000

    modified files

--- wiki/encyclopedia/views.py
+++ wiki/encyclopedia/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 
 from . import util
 
@@ -7,4 +7,20 @@ def index(request):
     return render(request, "encyclopedia/index.html", {
         "entries": util.list_entries()
     })
+
+def entry(request, title):
+    return render(request, "encyclopedia/entry.html", {
+        "title" : title,
+        "info" : util.get_entry(title),
+    } )
+
+def search(request):
+    q = request.GET['q']
+    entries = util.list_entries()
+    for entry in entries:
+        if q in entry:
+            return redirect("entry", title=q)
+    
+    return render(request, "encyclopedia/search.html")
+
 
